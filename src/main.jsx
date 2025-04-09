c122db7f24560af21ae72cb7d88f8f4f1fb0f20a
Sefukamil20R <Tenzilk20@gmail.com>
2025-04-10 02:49:14 +0300

    Cart page finalized

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import ProductDetails from './pages/Product_Details.jsx';
 import store from './redux/store.jsx';
 import './index.css'
 import App from './App.jsx'
-
+import CartPage from './pages/Cart.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <ProductDetails />  
+      {/* <ProductDetails />   */}
+      <CartPage />
       {/* <App /> */}
       <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} closeOnClick pauseOnHover draggable pauseOnFocusLoss />
     </Provider>
